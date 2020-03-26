c4d76150e9860eabc32d40d91563698af678b382
ashwinip5 <62322493+ashwinip5@users.noreply.github.com>
2020-03-26 21:03:17 +0530

    Update DIARashDriving.py

--- Function/DIARashDriving/DIARashDriving.py
+++ Function/DIARashDriving/DIARashDriving.py
@@ -1,13 +1,19 @@
 # Rash Driving detection
 import pandas as pd
-
-def detect_rash_driving(lat, long, pos_d, pos_e, Threshold):
+import numpy as np
+def detect_rash_driving(lat, long, pos_d, pos_e,Time, Threshold):
     x=[]
     y=[]
     z=[]
 
-    for i in range(0, len(lat)):
-        if (float(pos_d[i]) > Threshold) and (float(pos_e[i]) > Threshold / 2):
+    pos_d1 = pd.to_numeric(pos_d)
+    pos_e1 = pd.to_numeric(pos_e)
+    #print(pos_d1)
+    pos_d1=np.diff((pos_d1))/np.diff((Time))
+    #print(pos_d1)
+    pos_e1 = np.diff(pos_e1)/np.diff(Time)
+    for i in range(0, len(pos_d1)):
+        if (float(pos_d1[i]) > Threshold) and (float(pos_e1[i]) > Threshold / 2):
             x.append(lat[i])
             y.append(long[i])
             z.append(i)
