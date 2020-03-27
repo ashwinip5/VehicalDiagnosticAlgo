7dda942482c2330f65e0b74d16cb38172d1505fd
ashwinip5 <62322493+ashwinip5@users.noreply.github.com>
2020-03-27 12:01:32 +0530

    Update TestLoadAnalysis.py

--- Function/DIAEngineAnalysis_LoadAnalysis/TestLoadAnalysis.py
+++ Function/DIAEngineAnalysis_LoadAnalysis/TestLoadAnalysis.py
@@ -17,8 +17,7 @@ class TestLoad_Analysis(unittest.TestCase):
 				TempFile3=TempFile1[-1].split('.')
 				TempFile4=str(TempFile3[0])
 				path=os.path.join("Result/" , TempFile4)
-				if not os.path.isdir(path):
-					os.mkdir(path)
+
 				EngineLoadLess, EngineLoadMore, EngineRPMLess, EngineRPMMore, VehicleSpeedLess, VehicleSpeedMore, ExpectedSpeed,LoadThreshold,RPMThreshold,CounterOverload = DIAEngineAnalysis.LoadAnalysis(df["Engine Load(%)"].replace(to_replace='-',value=0), df["Engine RPM(rpm)"],df['Speed (GPS)(km/h)'].replace(to_replace='-',value=0),df['Trip Time(Since journey start)(s)'])
 				TempEngineLoad1=df["Engine Load(%)"].replace(to_replace='-',value=0)
 				plt.figure(1)
